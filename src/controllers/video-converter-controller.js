640a362b1db766b02e9bb4509039684f18e2c6b6
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-07 15:35:50 +0000

    Implementation of the route logic for processing and converting video links 1.0

---
+++ src/controllers/video-converter-controller.js
@@ -0,0 +1,12 @@
+// Importar a biblioteca ytdl-core para interagir facilmente com vídeos do YouTube
+// Import the ytdl-core library to easily interact with YouTube videos
+const ytdl =  require('ytdl-core');
+
+// Importar o módulo 'path' para manipulação de caminhos para os ficheiros
+// Import the ‘path’ module for manipulating file paths
+const path = require('path');
+
+// Importar o módulo 'fs' para leitura e escrita nos ficheiros
+// Import the 'fs' module for file reading and writing
+const fs = require('fs');
+
