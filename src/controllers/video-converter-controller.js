25dc5440e0c1208b8a058434fe51b92e6d64dd7d
Lodney Santos <a21505293@alunos.ulht.pt>
2025-01-08 00:25:31 +0000

    Implementation of the route logic for processing and converting video links 2.0

--- src/controllers/video-converter-controller.js
+++ src/controllers/video-converter-controller.js
@@ -10,3 +10,23 @@ const path = require('path');
 // Import the 'fs' module for file reading and writing
 const fs = require('fs');
 
+// expressão de função assíncrona atribuída a duas variáveis
+// cria uma vinculação de uma nova função assíncrona a um determinado nome.
+async function convertVideo(req, res) {
+    
+    const { videoUrl } = req.body;
+
+    // obrigatório fornecer o link do vídeo
+    if (!videoUrl) {
+        return res.status(400).json({ message: 'Video URL is required'});
+    }
+
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to process the video', error});
+    }
+}
+
+
+module.exports = { convertVideo };
--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@
 const app = require('.app/');
 
 // definir a Porta do servidor e tentar usar a PORTA definida na variável ambiente (process.env.PORT)
-/// mas, caso não esteja definida, é utilizada a porta padrão 3000
+// mas, caso não esteja definida, é utilizada a porta padrão 3000
 
 // set the server PORT and try to use the PORT defined in the environment variable (process.env.PORT)
-/// but if it is not set, the default port 3000 is used
+// but if it is not set, the default port 3000 is used
 const PORT = process.env.PORT || 3000;
 
 // Iniciar o servidor e escutar a porta
