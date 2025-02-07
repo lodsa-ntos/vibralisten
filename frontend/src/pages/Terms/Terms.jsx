c10811bdf0bb3e32ebb9980377fe55a5c109aed1
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-07 13:43:48 +0000

    Refactor Terms page; enhance styling with new CSS classes, improve semantic structure, and update content for clarity and accessibility

--- frontend/src/pages/Terms/Terms.jsx
+++ frontend/src/pages/Terms/Terms.jsx
@@ -15,10 +15,10 @@ const Terms = () => {
 
         <div className="terms-container">
 
-          <h1>Terms of use</h1>
+          <h1 className="terms-of-use-title">Terms of use</h1>
 
           <div className="terms-box">
-            <p>
+            <p className="text-about-terms">
               Welcome to <strong>VIBRALISTEN</strong>! These Terms of Use govern access to and use
               of our website, which offers an online tool for converting YouTube
               videos into MP3 files. By using this service, you agree to these
@@ -27,66 +27,66 @@ const Terms = () => {
             </p>
           </div>
 
-          <h2>1. Use of Service</h2>
+          <h2 className="subtitle-terms-section" >1. Use of Service</h2>
           <ul className="service-topcs">
-            <li>Acceptance of Terms</li>
+            <li className="terms-brands">Acceptance of Terms</li>
 
-            <p>
+            <p className="text-about-terms">
             By accessing this website, you agree to comply with these terms. If you do not agree, please do not use our services.
             </p>
 
-            <li>User Responsibility</li>
+            <li className="terms-brands">User Responsibility</li>
 
-            <p>
+            <p className="text-about-terms">
               You are solely responsible for your use of the service. You agree not to misuse our platform and to respect copyright laws when downloading content.
             </p>
 
-            <li>Legal Use</li>
+            <li className="terms-brands">Legal Use</li>
 
-            <p>
+            <p className="text-about-terms">
               The service should only be used for legal purposes. Any use that violates copyright, intellectual property laws or other regulations will be the sole responsibility of the user.
             </p>
           </ul>
 
-          <h2>2. Intellectual property</h2>
-          <p>
+          <h2 className="subtitle-terms-section">2. Intellectual property</h2>
+          <p className="text-about-terms">
           All content, design, graphics, logos and other elements on the website are the property of VIBRALISTEN or its licensors. Unauthorised use of these materials may infringe copyright laws and other proprietary rights.
           </p>
 
-          <h2>3. Limitation of Liability</h2>
+          <h2 className="subtitle-terms-section">3. Limitation of Liability</h2>
           <ul className="service-topcs">
-            <li>Conversion and Quality</li>
+            <li className="terms-brands">Conversion and Quality</li>
 
-            <p>
+            <p className="text-about-terms">
             <strong>VIBRALISTEN</strong> endeavours to provide high quality conversions, but does not guarantee that the process will be free of errors or interruptions.
             </p>
 
-            <li>Exclusion of Liability</li>
+            <li className="terms-brands">Exclusion of Liability</li>
 
-            <p>
+            <p className="text-about-terms">
             In no event shall <strong>VIBRALISTEN</strong> be liable for direct, indirect, incidental or consequential damages arising out of the use of the service or the inability to use it.
             </p>
 
-            <li>Changes to the Service</li>
+            <li className="terms-brands">Changes to the Service</li>
 
-            <p>
+            <p className="text-about-terms">
             We reserve the right to modify or discontinue the service at any time without prior notice.
             </p>
           </ul>
 
-          <h2>4. Modifications to the Terms</h2>
-          <p>
+          <h2 className="subtitle-terms-section">4. Modifications to the Terms</h2>
+          <p className="text-about-terms">
           We may update these Terms of Use from time to time. Changes will be posted on the site and, by continuing to use the service, you agree to the revised terms.
           </p>
 
-          <h2>5. Changes to Terms</h2>
-          <p>
+          <h2 className="subtitle-terms-section">5. Changes to Terms</h2>
+          <p className="text-about-terms">
           We may update these terms from time to time. The latest version will always be available on this page.
           </p>
 
           <div className="terms-box">
-            <p>
-              If you have any questions or suggestions about these Terms of Use, please contact us via our contact form or send an e-mail to <a href="mailto:support@vibralisten.com">support@vibralisten.com</a>.
+            <p className="text-about-terms">
+              If you have any questions or suggestions about these Terms of Use, please contact us via our contact form or send an e-mail to <a className="mail-support" href="mailto:support@vibralisten.com">support@vibralisten.com</a>.
             </p>
           </div>
           
