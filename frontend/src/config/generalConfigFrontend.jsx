9a5be5f72cfead9a343fb3780a621f002a90a978
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-14 05:14:45 +0000

    Update API and CSRF base URLs for production environment

--- frontend/src/config/generalConfigFrontend.jsx
+++ frontend/src/config/generalConfigFrontend.jsx
@@ -1,10 +1,10 @@
 const API_BASE_URL = process.env.NODE_ENV === "production" 
-? "https://vibralisten.com/api" 
+? "https://vibralisten-backend.onrender.com/api" 
 : "http://localhost:3000/api";
 
 
 const CSRF_BASE_URL = process.env.NODE_ENV === "production" 
-? "https://vibralisten.com" 
+? "https://vibralisten-backend.onrender.com" 
 : "http://localhost:3000";
 
 export { API_BASE_URL, CSRF_BASE_URL };
