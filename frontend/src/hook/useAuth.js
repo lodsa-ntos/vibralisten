a9032ed1269f51b0a69be63c1b590568b7ba228c
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-19 04:26:39 +0000

    Add useAuth hook to manage user authentication and redirect on login

---
+++ frontend/src/hook/useAuth.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from " react";
+
+/*
+* Ao entrar no site, o utilizador será redirecionado automaticamente para a home caso esteja logado.
+* When entering the site, the user will be automatically redirected to the home page if they are logged in.
+*/
+const useAuth = () => {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const token = localStorage.getItem("vibraToken");
+        const storedUser = localStorage.getItem("vibraUser");
+
+        if (token && storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+    });
+
+    return { user };
+};
+    
+export default useAuth;
