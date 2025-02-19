c61c72704bb45a7eda008b926e1d29c2ca41f42d
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-19 16:22:55 +0000

    Add AuthContext and AuthProvider for user authentication management

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect, Children } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ Children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("authToken");
+    const storedUser = localStorage.getItem("authUser");
+
+    if (token && storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  const login = (userData, token) => {
+    setUser(userData);
+    localStorage.setItem("authToken", token);
+    localStorage.setItem("authUser", JSON.stringify(userData));
+  };
+
+  const logout = (userData, token) => {
+    setUser(null);
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("authUser");
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {Children}
+    </AuthContext.Provider>
+  );
+};
