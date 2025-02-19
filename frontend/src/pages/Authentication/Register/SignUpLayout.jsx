9214e99a83d0aebcb94d762bd8dcdae2d5507796
Lodney Santos <a21505293@alunos.ulht.pt>
2025-02-19 21:01:56 +0000

    Refactor SignUpLayout to handle input changes and display loading state during registration

--- frontend/src/pages/Authentication/Register/SignUpLayout.jsx
+++ frontend/src/pages/Authentication/Register/SignUpLayout.jsx
@@ -21,7 +21,11 @@ const SignUpLayout = () => {
     password: ""
   });
 
-  handleSignUp = async () => {
+  const handleChange = async (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSignUp = async () => {
     setIsLoading(true);
     setError(null);
     
@@ -64,12 +68,14 @@ const SignUpLayout = () => {
         <h3>Discover new music. <br/>Join <span className="overlay-word-vibran">VIBRA</span>LISTE<span className="overlay-word-vibran">N</span> now!</h3>
         <div className="overlay-signup-content">
           <div className="overlay-content-signup-username">
-            <input type="email" id="signup-username-email" name="signup-username" className="peer block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600peer block w-full rounded-lg border border-gray-300 bg-transparent px-2 pt-5 pb-2 text-gray-900 dark:border-gray-600 dark:text-white focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" " required></input>
+            <input type="email" id="signup-username-email" name="signup-username" className="peer block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600peer block w-full rounded-lg border border-gray-300 bg-transparent px-2 pt-5 pb-2 text-gray-900 dark:border-gray-600 dark:text-white focus:border-blue-600 focus:outline-none focus:ring-0" onChange={handleChange} placeholder=" " required></input>
             <label htmlFor="signup-username-email" className="absolute left-2 top-2 text-sm text-gray-500 dark:text-gray-400 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-[-50%] peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:text-blue-600">E-mail or username</label>
           </div>
 
           <div className="overlay-content-signup-button">
-            <button>Let's vibe! <TbMusicShare size={20} style={{ marginLeft: "5px", color: "white"}}/></button>
+            <button onClick={handleSignUp} disabled={isLoading} > {isLoading ? "Registering..." : "Let's vibe!"} <TbMusicShare size={20} style={{ marginLeft: "5px", color: "white"}}/></button>
+
+            {error && <p>{error}</p>}
           </div>
           
           <div className="overlay-signup-content-divider">
