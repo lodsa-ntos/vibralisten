f9a9ebf015a2dca119990b9235d83a104ec9e501
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-03 18:12:43 +0000

    Refactor Tailwind CSS configuration to use CSS variables for colors; add global styles for improved theming

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,19 +4,16 @@ export default {
   theme: {
     extend: {
       colors: {
-        "blue-700": "#3d55cc",
-        "dark-grey": "#141a33",
-        "grey": "#505673",
-        "grey2": "#878CA8",
-        "light-grey": "#dadef2",
-        "light-blue-grey2": "#f5f6fa",
-        "white": "#FFFFFF",
-        "black": "#0A0F1E",
-        "border-color": "#7c7c7c",
-        blue: {
-          light: '#85d7ff',
-          '700': '#3d55cc',
-          dark: '#009eeb',
+        primary: {
+          50: "rgb(var(--blue-force-white) / <alpha-value>)",
+          100: "rgb(var(--blue-force-100) / <alpha-value>)",
+          200: "rgb(var(--blue-force-200) / <alpha-value>)",
+          300: "rgb(var(--blue-force-300) / <alpha-value>)",
+          400: "rgb(var(--blue-force-400) / <alpha-value>)",
+          500: "rgb(var(--blue-force-500) / <alpha-value>)",
+          600: "rgb(var(--blue-force-600) / <alpha-value>)",
+          700: "rgb(var(--blue-force-700) / <alpha-value>)",
+          800: "rgb(var(--blue-force-800) / <alpha-value>)",
         },
       },
       fontFamily: {
