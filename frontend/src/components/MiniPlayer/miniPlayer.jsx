5948447b13e7e408e1d5b43fa9c6367535e4697e
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-04 18:02:00 +0000

    Update MiniPlayer component styles for improved aesthetics and usability

--- frontend/src/components/MiniPlayer/miniPlayer.jsx
+++ frontend/src/components/MiniPlayer/miniPlayer.jsx
@@ -15,9 +15,8 @@ export const MiniPlayer = () => {
   };
 
   return(
-    <div className={`fixed bottom-4 right-4 ${isPlaying? "glow" : ""} flex items-center space-x-2 bg-[#3d55cc] text-white py-2 px-4 rounded-full shadow-lg hover:scale-105 transition-all`}>
+    <div className={`fixed bottom-4 right-4 ${isPlaying? "glow" : ""} flex items-center space-x-2 bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 text-white py-2 px-4 rounded-full shadow-lg hover:scale-105 transition-all z-50`}>
       <div className="flex items-center space-x-4">
-        <img src="https://via.placeholder.com/50" alt="Song Cover" className="w-12 h-12 rounded-lg" />
         <button onClick={togglePlay} className="flex items-center space-x-2">
           {isPlaying ? <Pause size={20} /> : <Play size={20} />}
           <span className='text-sm font-semibold'>Play a vibe</span>
