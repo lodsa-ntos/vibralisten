0ade7cfd49374e95ca96a5b2b9d2446d99b4a06d
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-05 18:00:12 +0000

    Enhance AuthProvider with signup functionality and refactor login/logout methods for improved navigation

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,39 +1,67 @@
 import { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => { 
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
+  // Verifica se existe uma sessão salva no localStorage
+  // Checks if there is a session saved in localStorage
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
   }, []);
-  
-  const login = (userData, token) => {
-    setUser(userData);
-    localStorage.setItem("user", JSON.stringify(userData));
-    localStorage.setItem("token", token);
 
-    setTimeout(() => {
-      window.location.href = "/home"
-    }, 0);
+  // Função de Signup
+  // Signup function
+  const signup = async (formData) => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/auth/signup", formData);
+      setUser(response.data.user);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      // Redirecionar após login
+      // Redirect after login
+      navigate("/home");
+
+    } catch (error) {
+      console.error("Signup error: ", error.response.data.message);
+      throw new Error(error.response.data.message || "Signup failed");
+    }
+  };
+  
+  // Função de Login
+  // Login function
+  const login = async (formData) => {
+    try {
+      const response = await axios.post("http://localhost:3000/api/auth/login", formData);
+      setUser(response.data.user);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+      // Redirecionar após login
+      // Redirect after login
+      navigate("/home");
+
+    } catch (error) {
+      console.error("Login error: ", error.response.data.message);
+      throw new Error(error.response.data.message || "Login failed");
+    }
   };
 
+  // Função de Logout
+  // Logout
   const logout = () => {
     setUser(null);
     localStorage.removeItem("user");
     localStorage.removeItem("token");
-
-    setTimeout(() => {
-      window.location.href = "/"
-    }, 0);
+    navigate("/login");
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, signup, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
