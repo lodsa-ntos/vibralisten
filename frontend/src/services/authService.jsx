c9233616c483f1a7cad81760fcd0aab30f23e966
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-08 14:50:52 +0000

    Implement loginUser function in authService for user authentication

---
+++ frontend/src/services/authService.jsx
@@ -0,0 +1,21 @@
+export const loginUser = async (loginInput) => {
+  try {
+    const response = await fetch("https://localhost:3000/api/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ login: loginInput }),
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      return data;
+    } else {
+      throw new Error(data.message || "Login failed");
+      
+    }
+  } catch (error) {
+    throw new Error(data.message || "Network error");
+    
+  }
+};
