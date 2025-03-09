121f89785ab95060ac99cbc5d657bee713f2b87f
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-09 23:08:42 +0000

    Refactor AuthContext to streamline session management; replace localStorage checks with active session verification and enhance logout functionality

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,84 +1,51 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => { 
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
 
-  // Verifica se existe uma sessão salva no localStorage
-  // Checks if there is a session saved in localStorage
-  useEffect(() => {
-    console.log("🔄 Checking localStorage for user...");
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      console.log("✅ User found in localStorage: ", JSON.parse(storedUser));
-      setUser(JSON.parse(storedUser));
-      console.log("User set from localStorage: ",JSON.parse(storedUser));
-    } else {
-      console.log("❌ No user found in localStorage.");
+  // verificar sessão ativa
+  // check active session
+  const checkSession = async () => {
+    try {
+      console.log("🔄 Checking session...");
+      const response = await axios.get("http://localhost:3000/api/auth/sessions", { withCredentials: true });
+
+      if (response.data && response.data.user) {
+        setUser(response.data.user);
+        console.log("✅ Session found, user authenticated: ", response.data.user);
+      } else {
+        setUser(null);
+        console.log("❌ No active session found.");
+      }
+    } catch (error) {
+      console.error("⚠️ Error checking session: ", error);
+      setUser(null);
     }
     setIsLoading(false);
-  }, []);
-
-  // Função de Signup
-  // Signup function
-  const signup = async (formData) => {
-    try {
-      const response = await axios.post("http://localhost:3000/api/auth/signup", formData);
-      setUser(response.data.user);
-      localStorage.setItem("user", JSON.stringify(response.data.user));
-
-    } catch (error) {
-      console.error("Signup error: ", error.response.data.message);
-      throw new Error(error.response.data.message || "Signup failed");
-    }
-  };
-  
-  // Função de Login
-  // Login function
-  const login = async (formData) => {
-    try {
-      const response = await axios.post("http://localhost:3000/api/auth/login", formData);
-      if (response.data.user) {
-        console.log("✅ Login successful. User: ", response.data.user);
-
-        setUser(response.data.user);
-        console.log("User form: ", formData);
-        localStorage.setItem("user", JSON.stringify(response.data.user));
-
-        navigate(`/home`);
-      } else {
-        throw new Error("User data is missing");
-      }
-
-    } catch (error) {
-      console.error("❌ Login error: ", error.response.data.message || error.message);
-      throw new Error(error.response.data.message || "Login failed");
-    }
-  };
-
-  // Função de Logout
-  // Logout
-  const logout = () => {
-    console.error("🔴 Logging out...");
-    setUser(null);
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    navigate("/login");
   };
 
   useEffect(() => {
-    if (user) {
-      navigate("/home");
+    checkSession();
+  }, []);
+  
+  // Função de Logout
+  // Logout
+  const logout = async () => {
+    try {
+      await axios.get("http://localhost:3000/api/auth/logout", {});
+
+      console.log("✅ User logged out.");
+    } catch (error) {
+      console.error("❌ Error logging out: ", error);
     }
-  }, [user, navigate]);
+  };
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated: !!user, login, signup, logout, isLoading }}>
+    <AuthContext.Provider value={{ user, isAuthenticated: !!user, isLoading, setUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
