5f514f0ab87ab47162ba5d1f17fc5c8af9484b4d
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-20 21:13:17 +0000

    Wrap App component with AuthProvider for centralized authentication context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import App from "./App.jsx";
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { AuthProvider } from './provider/authProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </BrowserRouter>
 );
