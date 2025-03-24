4cc4352562b34af4b31ff5fab66c38d040ace566
Lodney Santos <a21505293@alunos.ulht.pt>
2025-03-24 22:43:50 +0000

    Refactor UserHome component; enhance sidebar menu with new icons and improve styling

--- frontend/src/pages/Home/UserHome.jsx
+++ frontend/src/pages/Home/UserHome.jsx
@@ -5,6 +5,9 @@ import { getCsrfToken } from "../../utils/getCsrfToken";
 import  { useAuth } from "../../provider/authProvider";import { WelcomeModal } from "../WelcomeModal/welcomeModal";
 import { PiSidebar } from "react-icons/pi";
 import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { TbHome } from "react-icons/tb";
+import { LiaMicrophoneAltSolid } from "react-icons/lia";
+import { RiMusicAiLine } from "react-icons/ri";
 
 export const UserHome = () => {
   const [showWelcomeModal, setShowWelcomeModal] = useState(false);
@@ -78,126 +81,103 @@ export const UserHome = () => {
   }, []);
 
   return (
-    <div className="flex w-full h-screen bg-[#f4f4f5]">
-
-      {/* Overlay in mobile */}
+    <div style={{ fontFamily: "Satoshi, sans-serif", letterSpacing: "0.5px" }} className="flex w-full h-screen bg-[#f4f4f5]">
+      {/* Overlay in mobile - por cima */}
       {visibleMenu && (
-        <div 
-          onClick={() => setVisibleMenu
-          (false)}
-          className="fixed inset-0 z-80 bg-gray-50/50 dark:bg-black/50 md:hidden">
-
-        </div>
+        <div onClick={() => setVisibleMenu(false)} className="fixed inset-0 z-80 bg-gray-50/50 dark:bg-black/50 md:hidden"/>
       )}
-          {/* Sidebar */}
+        {/* Sidebar */}
           {visibleMenu && (
             <div className={`transition-transform duration-500 ${visibleMenu ? "translate-x-0" : " -translate-x-full"} fixed top-0 left-0 pl-3 z-40 h-full w-[290px] bg-[#ededed] border-white/20 md:relative md:translate-x-0 md:w-[260px] flex flex-col`}>
 
             {/* fixed header */}
               <div className="sticky top-0 bg-[#ededed] z-50 h-[100px] items-center xs:pr-3 xs:pl-3 xs:pb-3 xs:pt-3 pt-4">
-              <div className="flex items-center justify-between px-1.5">
-              <button  onClick={() => setVisibleMenu(false)} >
+                <div className="flex items-center justify-between px-1.5">
+                  <button  onClick={() => setVisibleMenu(false)} >
+                    <span className="hidden md:inline">
+                      <PiSidebar size={24} className=" left-0 transition-opacity transform duration-300 ease-in-out hover:scale-105 text-gray-500" />
+                    </span>
 
-                <span className="hidden md:inline">
-                  <PiSidebar size={24} className=" left-0 transition-opacity transform duration-300 ease-in-out hover:scale-105" />
-                </span>
-
-                <span className="inline md:hidden text-xl">
-                  <HiMiniBars3BottomLeft size={24} className="left-0 transition-opacity transform duration-300 ease-in-out hover:scale-105" />
-                </span>
-              </button>
-              </div>
-              <h2 className="text-xl text-gray-500 font-bold px-1.5 pt-3">
-                  Menu
-                </h2>
+                    <span className="inline md:hidden text-xl">
+                      <HiMiniBars3BottomLeft size={24} className="left-0 transition-opacity transform duration-300 ease-in-out hover:scale-105 text-gray-500" />
+                    </span>
+                  </button>
+                </div>
+                {/* Title */}
+                <h2 className="text-xl text-gray-500 font-bold px-1.5 pt-3"> </h2>
               </div>
                 
+              {/* Scroll */}
               <div className="flex-1 overflow-y-auto px-1.5 py-2 [&::-webkit-scrollbar]:w-2
-              [&::-webkit-scrollbar-track]:rounded-full
-              [&::-webkit-scrollbar-track]:bg-[#ededed]
-              [&::-webkit-scrollbar-thumb]:rounded-full
-              [&::-webkit-scrollbar-thumb]:bg-gray-300
-              dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-              dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
-
-              <ul className="space-y-2 text-sm text-gray-500">
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
-                  <li>Menu Item 1</li>
-                  <li>Menu Item 2</li>
-                  <li>Menu Item 3</li>
-                  <li>Menu Item 4</li>
+                [&::-webkit-scrollbar-track]:rounded-full
+                [&::-webkit-scrollbar-track]:bg-[#ededed]
+                [&::-webkit-scrollbar-thumb]:rounded-full
+                [&::-webkit-scrollbar-thumb]:bg-gray-300
+                dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+                dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
+              >
+                {/* Menu items */}
+                <ul className="space-y-6 py-3 text-sm text-gray-500">
+                    <li className="flex items-center gap-2"><TbHome size={20} /> Home</li>
+                    <li className="flex items-center gap-2"><LiaMicrophoneAltSolid size={20} /> Artists</li>
+                    <li className="flex items-center gap-2"><RiMusicAiLine size={20} /> Songs</li> {/* // será trocado depois*/}
                 </ul>
-            </div>
+              </div>
 
-            <div className="px-1.5 py-4 bg-[#ededed]">
-              <span className="text-sm text-gray-500">
-                Footer fixo
-              </span>
-            </div>
+              {/* Footer */}
+              <div className="px-1.5 py-4 bg-[#ededed]">
+                <span className="text-sm text-gray-500">
+                  Footer fixo
+                </span>
+              </div>
             </div>
           )}
 
-          {/* Home content */}
-          <div className="flex-1">
-              {/* Menu button */}
-              {!visibleMenu && (
-                <button
-                onClick={() => setVisibleMenu(!visibleMenu)}
-                className={`fixed top-0 p-4 z-50 rounded transition-opacity transform duration-300 ease-in-out hover:scale-105`}
-              >
-                <span className="hidden md:inline">
-                  <PiSidebar size={24} />
-                </span>
+            {/* Home content */}
+            <div className="flex-1">
+                {/* Menu button */}
+                {!visibleMenu && (
+                  <button
+                  onClick={() => setVisibleMenu(!visibleMenu)}
+                  className={`fixed top-0 p-4 z-50 rounded transition-opacity transform duration-300 ease-in-out hover:scale-105`}
+                >
+                  <span className="hidden md:inline">
+                    <PiSidebar size={24} className="text-gray-500" />
+                  </span>
 
-                <span className="inline md:hidden text-xl">
-                  <HiMiniBars3BottomLeft size={24} />
-                </span>
-                
-              </button>
-              )}
-              
-          <h1 className="text-3xl p-6 font-semibold text-center mt-8"> Welcome to VibraListen! 🎵</h1>
+                  <span className="inline md:hidden text-xl">
+                    <HiMiniBars3BottomLeft size={24} className="text-gray-500" />
+                  </span>
+                  
+                </button>
+                )}
 
-            {/* Show Modal if first time */}
-            {showWelcomeModal && (
-              <WelcomeModal user={user} onclose={() => setShowWelcomeModal(false)} />
-            )}
+              {/* Home content */}
 
-            {/* Logout button */}
-            <div className="flex justify-center mt-8">
-              <button
-                onClick={handleLogout}
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-600 transition duration-200"
-              >
-                Logout
-              </button>
-            </div>  
-            
-         
-      </div>
+                {/* Logo in the top left corner */}
+                <div className="flex ">
+                  <img
+                    src="Logo/vibralisten_logo_w.svg"
+                    className={`${visibleMenu ? "md:ml-2" : "ml-12" } w-24 py-3 dark:hidden inline-block object-contain drop-shadow-[0px_4px_10px_rgba(255,255,255,0.4)]`}
+                    alt="vibralisten_logo"
+                  />
+                </div>
+                  
+                {/* Welcome message */}
+                <h1 className="text-3xl p-6 font-semibold text-center mt-8"> Welcome to VibraListen! 🎵</h1>
+
+                {/* Show Modal if first time */}
+                {showWelcomeModal && ( <WelcomeModal user={user} onclose={() => setShowWelcomeModal(false)} />)}
+
+                {/* Logout button */}
+                <div className="flex justify-center mt-8">
+                  <button onClick={handleLogout}
+                    className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-600 transition duration-200"
+                  >
+                    Logout
+                  </button>
+                </div> 
+            </div>
       <ScrollToTopButton />
     </div>
   );
